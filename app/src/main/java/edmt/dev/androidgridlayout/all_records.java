d0a533916407258d5babb1921466d092d48c8e63
Robert <robertdaniel.cook@my.jcu.edu.au>
2019-04-26 17:05:50 +0800

    added admin pages

---
+++ app/src/main/java/edmt/dev/androidgridlayout/all_records.java
@@ -0,0 +1,13 @@
+package edmt.dev.androidgridlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class all_records extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_records);
+    }
+}
---
+++ app/src/main/java/edmt/dev/androidgridlayout/create_record.java
@@ -0,0 +1,13 @@
+package edmt.dev.androidgridlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class create_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_record);
+    }
+}
